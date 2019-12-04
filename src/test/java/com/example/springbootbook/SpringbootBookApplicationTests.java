6847d25b9a093ecec1d04ced57285a4852dea871
Junha2 <jhkim@popcornsar.com>
2019-12-04 19:46:46 +0900

    first commit

---
+++ src/test/java/com/example/springbootbook/SpringbootBookApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.springbootbook;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringbootBookApplicationTests {
+
+
+}
---
+++ src/test/java/com/example/springbootbook/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.springbootbook.domain.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class PostsRepositoryTest {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @Test
+    public void test_게시판저장_불러오기() throws Exception {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jhkim")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
